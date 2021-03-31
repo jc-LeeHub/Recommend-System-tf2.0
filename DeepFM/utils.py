c067afecbc12b769d23e59607f3ad4f290035817
jc_Lee <13829731389@163.com>
2021-03-31 23:39:43 +0800

    Update utils.py

--- DeepFM/utils.py
+++ DeepFM/utils.py
@@ -3,12 +3,17 @@
 # Author : junchaoli
 # File   : utils.py
 '''
-
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-def create_criteo_dataset(file_path, test_size=0.3):
+def sparseFeature(feat, feat_onehot_dim, embed_dim):
+    return {'feat': feat, 'feat_onehot_dim': feat_onehot_dim, 'embed_dim': embed_dim}
+
+def denseFeature(feat):
+    return {'feat': feat}
+
+def create_criteo_dataset(file_path, embed_dim=8, test_size=0.2):
     data = pd.read_csv(file_path)
 
     dense_features = ['I' + str(i) for i in range(1, 14)]
@@ -21,11 +26,15 @@ def create_criteo_dataset(file_path, test_size=0.3):
     #归一化
     data[dense_features] = MinMaxScaler().fit_transform(data[dense_features])
     #LabelEncoding编码
-    data = pd.get_dummies(data)
+    for col in sparse_features:
+        data[col] = LabelEncoder().fit_transform(data[col])
+
+    feature_columns = [[denseFeature(feat) for feat in dense_features]] + \
+           [[sparseFeature(feat, data[feat].nunique(), embed_dim) for feat in sparse_features]]
 
     #数据集划分
     X = data.drop(['label'], axis=1).values
     y = data['label']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
 
-    return (X_train, y_train), (X_test, y_test)
+    return feature_columns, (X_train, y_train), (X_test, y_test)

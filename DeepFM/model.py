828e727180fe9336a83347559b55573ab4da6e8d
jc_Lee <13829731389@163.com>
2021-03-31 23:38:57 +0800

    Update model.py

--- DeepFM/model.py
+++ DeepFM/model.py
@@ -7,15 +7,28 @@ from layer import FM_layer, Dense_layer
 
 import tensorflow as tf
 from tensorflow.keras import Model
+from tensorflow.keras.layers import Embedding
 
 class DeepFM(Model):
-    def __init__(self, k, w_reg, v_reg, hidden_units, output_dim, activation):
+    def __init__(self, feature_columns, k, w_reg, v_reg, hidden_units, output_dim, activation):
         super().__init__()
+        self.dense_feature_columns, self.sparse_feature_columns = feature_columns
+        self.embed_layers = {
+            'embed_' + str(i): Embedding(feat['feat_onehot_dim'], feat['embed_dim'])
+             for i, feat in enumerate(self.sparse_feature_columns)
+        }
+        
         self.FM = FM_layer(k, w_reg, v_reg)
         self.Dense = Dense_layer(hidden_units, output_dim, activation)
 
     def call(self, inputs):
-        fm_output = self.FM(inputs)
-        dense_output = self.Dense(inputs)
+        dense_inputs, sparse_inputs = inputs[:, :13], inputs[:, 13:]
+        # embedding
+        sparse_embed = tf.concat([self.embed_layers['embed_{}'.format(i)](sparse_inputs[:, i])
+                                  for i in range(sparse_inputs.shape[1])], axis=1)
+        x = tf.concat([dense_inputs, sparse_embed], axis=-1)
+
+        fm_output = self.FM(x)
+        dense_output = self.Dense(x)
         output = tf.nn.sigmoid(0.5*(fm_output + dense_output))
         return output

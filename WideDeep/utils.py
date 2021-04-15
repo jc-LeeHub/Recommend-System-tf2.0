4f6031f353d0bed28ff297766b4791e58c9dec99
jc_Lee <13829731389@163.com>
2021-04-15 23:03:42 +0800

    Update utils.py

--- WideDeep/utils.py
+++ WideDeep/utils.py
@@ -20,22 +20,30 @@ def create_criteo_dataset(file_path, embed_dim=8, test_size=0.2):
     dense_features = ['I' + str(i) for i in range(1, 14)]
     sparse_features = ['C' + str(i) for i in range(1, 27)]
 
+    y = data['label']
+    X = data.drop(['label'], axis=1)
+
     #缺失值填充
-    data[dense_features] = data[dense_features].fillna(0)
-    data[sparse_features] = data[sparse_features].fillna('-1')
+    X[dense_features] = X[dense_features].fillna(0)
+    X[sparse_features] = X[sparse_features].fillna('-1')
 
     #归一化
-    data[dense_features] = MinMaxScaler().fit_transform(data[dense_features])
-    #LabelEncoding编码
+    X[dense_features] = MinMaxScaler().fit_transform(X[dense_features])
+
+    #Onehot编码(wide侧输入)
+    onehot_data = pd.get_dummies(X)
+
+    #LabelEncoding编码(deep侧输入)
     for col in sparse_features:
-        data[col] = LabelEncoder().fit_transform(data[col])
+        X[col] = LabelEncoder().fit_transform(X[col])
+
+    # 拼接到数据集供wide使用
+    X = pd.concat([X, onehot_data], axis=1)
 
     feature_columns = [[denseFeature(feat) for feat in dense_features]] + \
-           [[sparseFeature(feat, data[feat].nunique(), embed_dim) for feat in sparse_features]]
+           [[sparseFeature(feat, X[feat].nunique(), embed_dim) for feat in sparse_features]]
 
-    #数据集划分
-    X = data.drop(['label'], axis=1).values
-    y = data['label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    # 数据集划分
+    X_train, X_test, y_train, y_test = train_test_split(X.values, y, test_size=test_size)
 
     return feature_columns, (X_train, y_train), (X_test, y_test)

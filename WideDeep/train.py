19a9450c88d3e161ac765f162d665f80d45cdabf
jc_Lee <13829731389@163.com>
2021-04-15 23:06:47 +0800

    Update train.py

--- WideDeep/train.py
+++ WideDeep/train.py
@@ -30,19 +30,21 @@ if __name__ == '__main__':
     # logloss, auc = model.evaluate((X_test[:, :13], X_test[:, 13:]), y_test)
     # print('logloss {}\nAUC {}'.format(round(logloss,2), round(auc,2)))
     # model.summary()
-
+    
+    # tensorboard可视化(不需要可以注释掉)
     summary_writer = tf.summary.create_file_writer('E:\\PycharmProjects\\tensorboard')
     for i in range(100):
         with tf.GradientTape() as tape:
             y_pre = model(X_train)
             loss = tf.reduce_mean(losses.binary_crossentropy(y_true=y_train, y_pred=y_pre))
             print(loss.numpy())
+        # 可视化
         with summary_writer.as_default():
             tf.summary.scalar("loss", loss, step=i)
         grad = tape.gradient(loss, model.variables)
         optimizer.apply_gradients(grads_and_vars=zip(grad, model.variables))
 
-#     #评估
-#     pre = model(X_test)
-#     pre = [1 if x>0.5 else 0 for x in pre]
-#     print("ACC: ", accuracy_score(y_test, pre))
+    #评估
+    pre = model(X_test)
+    pre = [1 if x>0.5 else 0 for x in pre]
+    print("ACC: ", accuracy_score(y_test, pre))
